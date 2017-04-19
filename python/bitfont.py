df9c93c58c483c1d82a793b17dc01b8ca0f1458c
Cefn Hoile <github.com@cefn.com>
2017-04-19 07:55:18 +0100

    Switched argument order to shorten default argument list. Added draw_generator which doesn't require serialisation of whole string

--- python/bitfont.py
+++ python/bitfont.py
@@ -61,12 +61,12 @@ class BitFont():
             maxX = max(lineX, maxX)
         return (maxX, len(lines) * self.height)
 
-    def draw_byte(self, ascii, x, y, plotter):
+    def draw_byte(self, ascii, plotter, x=0, y=0):
         """ Plot the points making up a character
         :param ascii: the character as a ascii byte
-        :param x: x coord of top left pixel
-        :param y: y coord of top left pixel
-        :param plotter: a function which will be called as plotter(x,y) for every character bit
+        :param plotter: a function called as plotter(x,y) for every character bit
+        :param x: x coord of top left pixel, default 0
+        :param y: y coord of top left pixel, default 0
         :return: the number of pixel columns used by the character
         """
         assert ascii >= firstAsciiChar and ascii < endAsciiChar
@@ -93,35 +93,61 @@ class BitFont():
                 raise e
         return dX
 
-    def draw_text(self, text, x, y, plotter):
+    def draw_line(self, text, plotter, x=0, y=0):
         """Plot a single line of characters
         :param text: the characters as a string
-        :param x: x coord of top left pixel
-        :param y: y coord of top left pixel
-        :param plotter: a function which will be called as plotter(x,y) for every character bit
+        :param plotter: a function called as plotter(x,y) for every character bit
+        :param x: x coord of top left pixel, default 0
+        :param y: y coord of top left pixel, default 0
         :return: the number of pixel columns used by the string
         """
         assert "\n" not in text
         dX = 0
         for char in text:
-            dX += self.draw_byte(ord(char), x + dX, y, plotter)
+            dX += self.draw_byte(ord(char), plotter, x + dX, y)
         return dX
 
-    def draw_para(self, para, x, y, plotter, lineHeight=None):
+    def draw_para(self, para, plotter, x=0, y=0, lineHeight=None):
         if lineHeight == None:
             lineHeight = self.height
         """Plot multiple lines of characters
         :param para: the lines as a string
-        :param x: x coord of top left pixel
-        :param y: y coord of top left pixel
         :param plotter: a function which will be called as plotter(x,y) for every character bit
+        :param x: x coord of top left pixel, default 0
+        :param y: y coord of top left pixel, default 0
         :return: (cols, rows) total pixels used by the text drawn
         """
         lines = para.split("\n")
         maxX = 0
         dY = 0
         for line in lines:
-            lineX = self.draw_text(line, x, y + dY, plotter)
+            lineX = self.draw_line(line, plotter, x, y + dY)
             maxX = max(lineX, maxX)
             dY += lineHeight
+        return (maxX, dY)
+
+    def draw_generator(self, generator, plotter, x=0, y=0, lineHeight=None):
+        """Plot either bytearray or str chunks provided by a generator
+        :param generator: the generator
+        :param plotter: a function called as plotter(x,y) for every character bit
+        :param x: x coord of top left pixel, default 0
+        :param y: y coord of top left pixel, default 0
+        :return: (cols, rows) total pixels used by the text drawn
+        """
+        maxX = 0
+        dX = 0
+        dY = 0
+        for chunk in generator:
+            chunkType = type(chunk)
+            assert chunkType==bytearray or chunkType==str
+            for char in chunk:
+                if char is ord('\n'):
+                    dY += lineHeight
+                    maxX = max(dX, maxX)
+                    dX = 0
+                else:
+                    dX += self.draw_byte(char, plotter, x + dX, y + dY)
+            if dX is not 0: # catch case that last line is partial/unterminated (duplicated newline detection code)
+                dY += lineHeight
+                maxX = max(dX, maxX)
         return (maxX, dY)
