a9f85f155645b5e7e842b9ce713a733640a805c9
Cefn Hoile <github.com@cefn.com>
2017-05-20 10:53:49 +0100

    Simplified test example

--- python/render/pillow.py
+++ python/render/pillow.py
@@ -1,20 +1,5 @@
 from PIL import Image,ImageDraw
 from os import chdir,path
-import string,re
-
-white = "white"
-black = "black"
-
-def normalise_color(color):
-    if color == white:
-        color = 1
-    elif color == black:
-        color = 0
-    assert color == 0 or color == 1, "Unrecognised color specification"
-    return color
-
-def normalise_space(text):
-    return "\n".join([line.strip() for line in text.split('\n')])
 
 class BitScreen():
 
@@ -31,12 +16,10 @@ class BitScreen():
 
 def run():
     from faces.font_5x7 import font
-    message = normalise_space(
-"""Pretend to CARRY the
+    message = """Pretend to CARRY the
 heavy bucket back to
 the main building
 Don't spill any"""
-    )
     (width, height) = [dim for dim in font.para_dims(message)]
     screen = BitScreen(width=width, height=height)
     plotter = screen.create_plotter()
